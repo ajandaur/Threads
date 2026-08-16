f2fa4d60d483869289a55ba6c2028d992c3ea98b
Anmol Jandaur <Anmol.jandaur@gmail.com>
2026-08-16 15:49:51 -0400

    Initial Commit

---
+++ ThreadsTests/ThreadsTests.swift
@@ -0,0 +1,19 @@
+//
+//  ThreadsTests.swift
+//  ThreadsTests
+//
+//  Created by Anmol  Jandaur on 8/16/26.
+//
+
+import Testing
+@testable import Threads
+
+struct ThreadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
